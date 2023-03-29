ed6f27141fab0c6e7e98bac97bc4d061969e7eaa
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-03-29 16:15:27 +0000

    commit

--- server-side/app/Providers/RouteServiceProvider.php
+++ server-side/app/Providers/RouteServiceProvider.php
@@ -18,7 +18,6 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
-    protected $namespace = 'App\\Http\\Controllers';
 
 
     /**
--- server-side/routes/web.php
+++ server-side/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UsersAuth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
