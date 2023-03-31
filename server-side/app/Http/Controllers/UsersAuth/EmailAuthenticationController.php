282714bca9426f2553f173f2d261d0dab10f9dc8
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-03-31 15:40:11 +0000

    work with multiple databases

--- server-side/app/Http/Controllers/UsersAuth/EmailAuthenticationController.php
+++ server-side/app/Http/Controllers/UsersAuth/EmailAuthenticationController.php
@@ -5,28 +5,38 @@ namespace App\Http\Controllers\UsersAuth;
 use App\Http\Controllers\Controller;
 use App\Interfaces\AuthenticationServiceInterface as AuthInterface;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class EmailAuthenticationController extends Controller implements AuthInterface
 {
     static public function register(Request $request)  :? JsonResponse
     {
-        $formFields = $request->validate([
-            'name' => ['required', 'min:4'],
-            'email' => ['required', 'email', '  unique:users'],
+        $formFields = Validator::make($request->all(), [
+            'name' => ['required', 'min:4', 'unique:macro_users.users'],
+            'email' => ['required', 'email', 'unique:macro_users.users'],
             'password' => ['required', 'min:8','regex:/^(?=.*[a-z])(?=.*[A-Z])/']
         ]);
         
         // Hash the password 
         $formFields['password'] = bcrypt($formFields['password']);
-        // Create user  
-        if(User::create($formFields)){
-            return response()->json(['Message' => 'User created seccussfully!', 201]);
-        }else{
-            return response()->json(['Message' => "There's a problem with your registration request ! " , 401]);
-        }
+        // Create user  and establich database connection 
+
+        $user = DB::connection('macro_users')->transaction(function () use ($formFields) {
+            $user = User::create([
+                'username' => $formFields['username'],
+                'email' => $formFields['email'],
+                'password' => Hash::make($formFields['password']),
+            ]);
+
+            return $user;
+        });
+        return response()->json(['user' => $user], 201);
+        
 
     }
 
