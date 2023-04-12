75594f3a65de45e40b6c12b6e237c7c340f538cc
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-04-12 16:26:59 +0000

    fetch the email login api using axios

--- client-side/src/components/Auth/LoginPage.jsx
+++ client-side/src/components/Auth/LoginPage.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import loginBg from '../../images/authentificationPages/loginBg.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faLock, faUser} from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function LoginPage() {
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLoginForm = (e) => {
+        e.preventDefault();
+        axios.post('http://127.0.0.1:8000/api/auth/login', { email, password,authenticationType: 'email' })
+            .then((response) => {
+                console.log(response)
+            })
+            .catch((error => {
+                console.error(` The erorr is : ${error}`);
+            }))
+    }
     return (
+
         <section className='w-screen h-screen bg-slate-950 flex flex-row justify-center items-center  gap-6 '>
             {/*  == Register page form == */}
             <div className="container bg-gradient-to-r from-[#D8D5D6] to-gray-500 w-[75%] h-[90%] flex rounded-3xl p-5">
                 <div className="register-form w-[70%] flex justify-center items-center">
-                    <form action="" className='flex flex-col justify-around'>
+                    <form onSubmit={handleLoginForm} className='flex flex-col justify-around'>
                     {/*  headers */}
 
                         <div className="user-greeting text-5xl font-bold mb-20">
@@ -20,7 +35,13 @@ function LoginPage() {
                     {/* == User name input section == */}
 
                         <div className="user-section my-4 mx-12 flex justify-center my-12 ">
-                            <input type="text" placeholder='Rabie Ouallaf' className='w-72 h-10 rounded-xl p-5 absolute text-center' />
+                            <input 
+                                type="email" 
+                                placeholder='Rabie@Ouallaf.com' 
+                                className='w-72 h-10 rounded-xl p-5 absolute text-center'
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}   
+                            />
                             <FontAwesomeIcon icon={faUser} className='relative top-3 right-32'/>
 
                         </div>
@@ -29,7 +50,13 @@ function LoginPage() {
 
 
                         <div className="password-section my-4 mx-12 flex justify-center my-12">
-                            <input type="password" placeholder='*********************************' className='w-72 h-10 rounded-xl p-5 absolute text-center' />
+                            <input 
+                                type="password" 
+                                placeholder='*********************************' 
+                                className='w-72 h-10 rounded-xl p-5 absolute text-center' 
+                                value={password}    
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
                             <FontAwesomeIcon icon={faLock} className='relative top-3 right-32'/>
                         </div>
 
