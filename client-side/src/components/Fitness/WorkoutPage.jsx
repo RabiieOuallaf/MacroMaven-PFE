48fc0b8da75a5699730ba6eb11bc46798afdd606
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-04-23 16:47:55 +0100

    exercise page

--- client-side/src/components/Fitness/WorkoutPage.jsx
+++ client-side/src/components/Fitness/WorkoutPage.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Sidebar from '../utils/main/Sidebar'
 import SideBarWokout from '../utils/fitness/SideBarWokout'
+import { Canvas } from '@react-three/fiber'
+import { MaleMain } from '../ThreeModels/MaleMain'
+import { OrbitControls } from '@react-three/drei'
 
 function WorkoutPage() {
-  return (
-    <section className='workout-page'>
-        <Sidebar color="slate-950"/>
-        <SideBarWokout />
-    </section>
-  )
+    return (
+        <section className='workout-page overflow-hidden'>
+            <Sidebar color="slate-950" />
+            <SideBarWokout />
+
+            <div className="male-anatomy ml-[30%] w-[1070px] h-[410px] flex justify-center items-center" >
+                <Canvas>
+                    <OrbitControls enableZoom={true} />
+                    <ambientLight intensity={0.5} />
+                    <directionalLight position={[-2, 5, 2]} intensity={1} />
+                    <Suspense fallback={null}>
+                        <MaleMain />
+                    </Suspense>
+
+                </Canvas>
+            </div>
+        </section>
+    )
 }
 
 export default WorkoutPage
