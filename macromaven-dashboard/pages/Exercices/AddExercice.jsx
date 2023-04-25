375b75c480090e7fce09fac0cc1891f11421f03c
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-04-25 09:55:09 +0100

    Add exercice page

---
+++ macromaven-dashboard/pages/Exercices/AddExercice.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+
+function AddExercice() {
+    return (
+        <div className="exercice-container bg-gray-900 min-h-screen p-10">
+            <section class="max-w-4xl p-6 mx-auto bg-indigo-600 rounded-md shadow-md dark:bg-gray-800 p-10">
+                <h1 class="text-xl font-bold text-white capitalize dark:text-white">Account settings</h1>
+                <form >
+                    <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                        <div>
+                            <label class="text-white dark:text-gray-200" for="username">Exercice name</label>
+                            <input id="username" type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
+                        </div>
+
+                        <div>
+                            <label class="text-white dark:text-gray-200" for="passwordConfirmation">Repetitions</label>
+                            <select class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
+                                <option>15</option>
+                                <option>12</option>
+                                <option>10</option>
+                                <option>8</option>
+                                <option>6</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <label class="text-white dark:text-gray-200" for="passwordConfirmation">Sets</label>
+                            <select class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <label class="text-white dark:text-gray-200" for="passwordConfirmation">Category</label>
+                            <select class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
+                                <option>Surabaya</option>
+                                <option>Jakarta</option>
+                                <option>Tangerang</option>
+                                <option>Bandung</option>
+                            </select>
+                        </div>
+                       
+                        
+                        
+                        <div>
+                            <label class="block text-sm font-medium text-white">
+                                Image
+                            </label>
+                            <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                                <div class="space-y-1 text-center">
+                                    <svg class="mx-auto h-12 w-12 text-white" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                                        <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                    <div class="flex text-sm text-gray-600">
+                                        <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                                            <span class="">Upload a file</span>
+                                            <input id="file-upload" name="file-upload" type="file" class="sr-only" />
+                                        </label>
+                                        <p class="pl-1 text-white">or drag and drop</p>
+                                    </div>
+                                    <p class="text-xs text-white">
+                                        PNG, JPG, GIF up to 10MB
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="flex justify-end mt-6">
+                        <button class="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-700 focus:outline-none focus:bg-gray-600">Save</button>
+                    </div>
+                </form>
+            </section>
+        </div>
+
+    )
+}
+
+export default AddExercice
--- macromaven-dashboard/pages/Exercices/Exercices.jsx
+++ macromaven-dashboard/pages/Exercices/Exercices.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { BsBackspaceFill } from 'react-icons/bs';
-import {AiFillEdit} from 'react-icons/ai';
-import {exercice} from '../public/images/exercice.png';
+import {AiFillEdit, AiOutlinePlusCircle} from 'react-icons/ai';
+import {exercice} from '@/public/images/exercice.png';
+import Sidebar from '@/components/Sidebar';
 
 function Exercices() {
     return (
         <div className='bg-gray-900 min-h-screen'>
+            <Sidebar />
             <div className='flex justify-between px-4 pt-4 ml-24'>
                 <h2 className='text-white font-semibold text-lg'>Exercices</h2>
                 <h2 className='text-white font-semibold text-lg'>Wlecome back, <span className='text-white font-bold'>Rabie</span></h2>
             </div>
 
-            <div className="p-4 mx-[12%] my-4">
+            <div className="p-4 mx-[12%] my-4 flex gap-5">
                 <div className="w-full m-auto- p-4 border rounded-lg bg-white overflow-y-auto">
                     <div className="my-3 font-semibold grid md:grid-cols-7 sm:grid-cols-6 grid-cols-5 items-center justify-between cursor-pointer">
                         <span>Exercice image</span>
@@ -37,14 +39,17 @@ function Exercices() {
                             <p className='hidden md:flex'>18</p>
                             <p className='hidden md:flex'>mediterranean</p>
                             <div className='sm:flex hidden justify-between items-center'>
-                                <p className='font-bold text-red-500'><BsBackspaceFill /></p>
-                                <p className='font-bold text-red-500'><AiFillEdit /></p>
-
-
+                                <p className='font-bold text-red-500'><BsBackspaceFill cursor={'pointer'}/></p>
+                                <p className='font-bold text-red-500'><AiFillEdit cursor={'pointer'}/></p>
                             </div>
                         </li>
                     </ul>
                 </div>
+
+                <div className="add-exercice">
+                    <AiOutlinePlusCircle color='white' size={30} cursor={'pointer'}/>
+                </div>
+                
             </div>
         </div>
     )
