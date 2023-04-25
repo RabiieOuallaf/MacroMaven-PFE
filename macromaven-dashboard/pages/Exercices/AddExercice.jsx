e106a2b43c2ae90d464f6dd75ffb8cbcbbfdb022
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-04-25 15:15:52 +0100

    Add exercice

--- macromaven-dashboard/pages/Exercices/AddExercice.jsx
+++ macromaven-dashboard/pages/Exercices/AddExercice.jsx
@@ -1,20 +1,106 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Swal from 'sweetalert2'
+import axios from 'axios';
+
 
 function AddExercice() {
+    const [repetition, setRepetition] = useState();
+    const [sets, setSets] = useState('');
+    const [category, setCategory] = useState('');
+    const [exerciceName, setExerciceName] = useState('');
+    const [image, setImage] = useState('');
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        axios.post('http://127.0.0.1:8000/api/fitness/addexercice', {
+            exercice_name : exerciceName, 
+            exercice_sets: sets, 
+            exercice_repetition : repetition,
+            exercice_category : category,
+            exercice_image : image,
+        })
+        .then( (response) => {
+            Swal.fire(
+                'Exercice added seccussfully!',
+                'success'
+              )
+
+            console.log(response)
+        })
+        .catch( (erorr) => {
+            console.log(erorr);
+        })
+    }
+
+    const handleRepetionChange = (e, selectBox) => {
+        switch(selectBox) {
+            case 'repetition': 
+                setRepetition(e.target.value);
+                break;
+            case 'category' : 
+                setCategory(e.target.value);
+                break;
+            case 'set': 
+                setSets(e.target.value);
+                break;
+            default: 
+            Swal.fire({
+                title: 'Error!',
+                text: 'Please check your options!',
+                icon: 'error',
+                confirmButtonText: 'Cool'
+            });
+            }
+    }
+
+    const handleExerciceNameChange = (e) => {
+        setExerciceName(e.target.value);
+    }
+
+    const handlePictureChange = (e) => {
+        const file = e.target.files[0];
+        let formData = new FormData();
+        formData.append('file', file);
+        formData.append('upload_preset', 'iih51pyv');
+
+        axios.defaults.withCredentials = false;
+
+        axios.post('https://api.cloudinary.com/v1_1/drfwsaasz/upload', formData, {
+            headers: 'Access-Control-Allow-Origin'
+        })
+            .then((response) => {
+                setImage(response.data.secure_url)
+                console.log(image);
+
+            })
+            .catch((erorr) => {
+                console.log(erorr)
+            })
+    }
+
+
+    
+
     return (
         <div className="exercice-container bg-gray-900 min-h-screen p-10">
             <section class="max-w-4xl p-6 mx-auto bg-indigo-600 rounded-md shadow-md dark:bg-gray-800 p-10">
                 <h1 class="text-xl font-bold text-white capitalize dark:text-white">Account settings</h1>
-                <form >
+                <form onSubmit={handleFormSubmit}>
                     <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
                         <div>
                             <label class="text-white dark:text-gray-200" for="username">Exercice name</label>
-                            <input id="username" type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
+                            <input 
+                                id="username" 
+                                type="text" 
+                                class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" 
+                                onChange={(e) => handleExerciceNameChange(e)}
+                                />
                         </div>
 
                         <div>
                             <label class="text-white dark:text-gray-200" for="passwordConfirmation">Repetitions</label>
-                            <select class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
+                            <select name="exercice_repetition" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" onChange={(e) => handleRepetionChange(e,'repetition')}>
                                 <option value={15}>15</option>
                                 <option value={12}>12</option>
                                 <option value={10}>10</option>
@@ -25,7 +111,7 @@ function AddExercice() {
 
                         <div>
                             <label class="text-white dark:text-gray-200" for="passwordConfirmation">Sets</label>
-                            <select class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
+                            <select name="exercice_sets" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" onChange={(e) => handleRepetionChange(e,'set')}>
                                 <option value={1}>1</option>
                                 <option value={2}>2</option>
                                 <option value={3}>3</option>
@@ -36,7 +122,7 @@ function AddExercice() {
 
                         <div>
                             <label class="text-white dark:text-gray-200" for="passwordConfirmation">Category</label>
-                            <select class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
+                            <select name="exercice_category"class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" onChange={(e) => handleRepetionChange(e,'category')} >
                                 <option>Surabaya</option>
                                 <option>Jakarta</option>
                                 <option>Tangerang</option>
@@ -58,7 +144,7 @@ function AddExercice() {
                                     <div class="flex text-sm text-gray-600">
                                         <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
                                             <span class="">Upload a file</span>
-                                            <input id="file-upload" name="file-upload" type="file" class="sr-only" />
+                                            <input id="file-upload" name="file-upload" type="file" class="sr-only" onChange={(e) => handlePictureChange(e)}/>
                                         </label>
                                         <p class="pl-1 text-white">or drag and drop</p>
                                     </div>
